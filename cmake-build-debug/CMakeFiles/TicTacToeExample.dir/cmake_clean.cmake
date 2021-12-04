5c0cd0dee966aac7b4fb217ec8ce42b0c85594c5
iamrecan <46672587+iamrecan@users.noreply.github.com>
2021-12-04 19:45:01 +0300

    Working Version

---
+++ cmake-build-debug/CMakeFiles/TicTacToeExample.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TicTacToeExample.dir/TicTacToe.cpp.obj"
+  "CMakeFiles/TicTacToeExample.dir/main.cpp.obj"
+  "TicTacToeExample.exe"
+  "TicTacToeExample.exe.manifest"
+  "TicTacToeExample.pdb"
+  "libTicTacToeExample.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TicTacToeExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
